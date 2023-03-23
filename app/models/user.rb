78159e7d2525ea0220d2bb99fd40c902be8e7921
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-03-23 09:10:02 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :email,              presence: true
+  validates :name,               presence: true
+  validates :encrypted_password, presence: true
+  validates :profile,            presence: true
+  validates :occupation,         presence: true
+  validates :position,           presence: true
+
+  has_many :prototypes
+  has_many :comments
+end
